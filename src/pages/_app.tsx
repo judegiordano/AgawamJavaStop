f7d196852c566b946176b535234cede299d32195
JudeBoy <judegiordano@gmail.com>
2021-05-16 11:58:46 -0400

    Add Log Rocket

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { AppProps } from "next/dist/next-server/lib/router/router";
+import LogRocket from "logrocket";
+LogRocket.init("nwnxka/agawam-java-stop");
 
 import "../styles/globals.css";
 
